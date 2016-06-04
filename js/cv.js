8b9d593a6c5e3209948fa7dbea3a0ac3b89b2d03
Sébastien Guillon <sebastienguillon@gmail.com>
2016-06-04 16:23:59 +0200

    Minor css fixes and BG resize on CV page

--- js/cv.js
+++ js/cv.js
@@ -1,115 +1,118 @@
 'use strict';
+(function() {
+  var DOMLoaded = function() {
 
-var DOMLoaded = function() {
+    var stars;
 
-  var stars;
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
 
-  var canvas = document.getElementById('canvas');
-  var ctx = canvas.getContext('2d');
-
-  var width = canvas.width = window.innerWidth;
-  var height = canvas.height = window.innerHeight;
-
-
-  var setCanvasFullSize = function() {
-    console.log('setCanvasFullSize');
-    requestAnimationFrame(drawBg);
-  }; // setCanvasFullSize
+    var width = canvas.width = window.innerWidth;
+    var height = canvas.height = window.innerHeight;
 
 
-  var drawBg = function() {
+    var setCanvasFullSize = function() {
+      width = canvas.width = window.innerWidth;
+      height = canvas.height = window.innerHeight;
 
-    var nbStars1 = 500;
-    var stars1 = [];
-
-    for(var i = 0; i < nbStars1; i++ ) {
-      var star = new Star({
-        width: width,
-        height: height,
-        radius: Math.random() / 1.5,
-        x: Math.random() * width,
-        y: Math.random() * height,
-        direction: Math.PI * 1.5,
-        speed: .3,
-        color: '#fff'
-      });
-      stars1.push(star);
-    }
-
-    var nbStars2 = 200;
-    var stars2 = [];
-    for(var i = 0; i < nbStars2; i++ ) {
-      var star = new Star({
-        width: width,
-        height: height,
-        radius: Math.random() * 1.2,
-        x: Math.random() * width,
-        y: Math.random() * height,
-        direction: Math.PI * 1.5,
-        speed: .5,
-        color: '#fff'
-      });
-      stars2.push(star);
-    }
-
-    var nbStars3 = 100;
-    var stars3 = [];
-    for(var i = 0; i < nbStars3; i++ ) {
-      var star = new Star({
-        width: width,
-        height: height,
-        radius: Math.random() * 2.5,
-        x: Math.random() * width,
-        y: Math.random() * height,
-        direction: Math.PI * 1.5,
-        speed: 1,
-        color: '#fff'
-      });
-      stars3.push(star);
-    }
-
-    stars = stars1.concat(stars2, stars3);
-  }; // drawBg
+      requestAnimationFrame(drawBg);
+    }; // setCanvasFullSize
 
 
-  var renderLoop = function() {
-    ctx.clearRect(0, 0, width, height);
+    var drawBg = function() {
 
-    for(var i = 0; i < stars.length; i++) {
-      var star = stars[i];
-      star.update();
+      var nbStars1 = 500;
+      var stars1 = [];
 
-      ctx.save();
-      ctx.beginPath();
-      ctx.arc(star.x, star.y, star.radius, 0, Math.PI * 2, false);
-      ctx.fillStyle = star.color;
-      ctx.fill();
-
-      if(star.y + star.radius < 0) {
-        star.y = height;
-        star.x = star.x + Math.random() * 30;
+      for(var i = 0; i < nbStars1; i++ ) {
+        var star = new Star({
+          width: width,
+          height: height,
+          radius: Math.random() / 1.5,
+          x: Math.random() * width,
+          y: Math.random() * height,
+          direction: Math.PI * 1.5,
+          speed: .3,
+          color: '#fff'
+        });
+        stars1.push(star);
       }
-      ctx.restore();
-    } // for
 
-    requestAnimationFrame(renderLoop);
-  } // renderLoop
+      var nbStars2 = 200;
+      var stars2 = [];
+      for(var i = 0; i < nbStars2; i++ ) {
+        var star = new Star({
+          width: width,
+          height: height,
+          radius: Math.random() * 1.2,
+          x: Math.random() * width,
+          y: Math.random() * height,
+          direction: Math.PI * 1.5,
+          speed: .5,
+          color: '#fff'
+        });
+        stars2.push(star);
+      }
 
-  drawBg();
-  renderLoop();
+      var nbStars3 = 100;
+      var stars3 = [];
+      for(var i = 0; i < nbStars3; i++ ) {
+        var star = new Star({
+          width: width,
+          height: height,
+          radius: Math.random() * 2.5,
+          x: Math.random() * width,
+          y: Math.random() * height,
+          direction: Math.PI * 1.5,
+          speed: 1,
+          color: '#fff'
+        });
+        stars3.push(star);
+      }
+
+      stars = stars1.concat(stars2, stars3);
+    }; // drawBg
+
+
+    var renderLoop = function() {
+      ctx.clearRect(0, 0, width, height);
+
+      for(var i = 0; i < stars.length; i++) {
+        var star = stars[i];
+        star.update();
+
+        ctx.save();
+        ctx.beginPath();
+        ctx.arc(star.x, star.y, star.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = star.color;
+        ctx.fill();
+
+        if(star.y + star.radius < 0) {
+          star.y = height;
+          star.x = star.x + Math.random() * 30;
+        }
+        ctx.restore();
+      } // for
+
+      requestAnimationFrame(renderLoop);
+    } // renderLoop
+
+    drawBg();
+    renderLoop();
 
 
 
-  var windowResizeHandler = function() {
-    setCanvasFullSize();
-  } // windowResizeHandler
-  window.addEventListener('resize', windowResizeHandler);
+    var windowResizeHandler = function() {
+      setCanvasFullSize();
+    } // windowResizeHandler
+    window.addEventListener('resize', windowResizeHandler);
 
 
-  document.getElementById('play-asteroids').addEventListener('click', function() {
-    window.location = '/';
-  });
+    document.getElementById('play-asteroids').addEventListener('click', function() {
+      window.location = '/';
+    });
 
-} // DOMLoaded
+  } // DOMLoaded
 
-window.addEventListener('DOMContentLoaded', DOMLoaded);
+  window.addEventListener('DOMContentLoaded', DOMLoaded);
+}());
